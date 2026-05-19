ba678570b150f58b3c14a53c9ecb09608425d165
Dharmi Chadamiya <dharmichadamiya@gmail.com>
2026-05-19 10:04:15 +0530

    fix db connection timing

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,27 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express();
-const port = process.env.PORT || 10000;
-const connectDB = require("./connect");
-connectDB();
-const noteRoute = require("./routes/noteRoutes");
+
 app.use(express.json());
 app.use(cors());
+
+const noteRoute = require("./routes/noteRoutes");
 app.use("/", noteRoute);
 
-app.listen(port, () => {
-  console.log("server is listening on port " + port);
+const port = process.env.PORT || 10000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(port, () => {
+        console.log("server is listening on port " + port);
+    });
+})
+.catch((err) => {
+    console.log(err);
 });
