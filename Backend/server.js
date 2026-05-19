a43f130c644660f293d6cff5b8a8b3be1f67538a
Dharmi Chadamiya <dharmichadamiya@gmail.com>
2026-05-19 09:56:11 +0530

    fix render deployment stability

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
-const port = process.env.PORT;
-require("./connect");
-const noteRoute = require("./routes/noteRoutes");
 const cors = require("cors");
-
+const app = express();
+const port = process.env.PORT || 10000;
+const connectDB = require("./connect");
+connectDB();
+const noteRoute = require("./routes/noteRoutes");
 app.use(express.json());
 app.use(cors());
-app.use("/",noteRoute);
+app.use("/", noteRoute);
 
-app.listen(port,()=>{
-    console.log('server is listening on port' + port);
+app.listen(port, () => {
+  console.log("server is listening on port " + port);
 });
